ae0694132d76b8cf2428828f7be59ad4a755e555
VanehSann <vanessacbsantos22@gmail.com>
2022-01-27 15:56:37 -0300

    requisito 2 + lint

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,16 +3,12 @@ const data = require('../data/zoo_data');
 function getAnimalsOlderThan(animal, ages) {
   // seu código aqui
   // console.log(data.species[0].residents[2].age)
-for(let index = 0; index < data.species.length; index += 1) {
-  if(data.species[index].name === animal) {
-    
-    const residentData = data.species[index].residents.every((resident) => {
-      return resident.age >= ages
-    });
-    return residentData
+  for (let index = 0; index < data.species.length; index += 1) {
+    if (data.species[index].name === animal) {
+      const residentData = data.species[index].residents.every((resident) => resident.age >= ages);
+      return residentData;
+    }
   }
-}
-
 }
 
 // console.log(getAnimalsOlderThan('otters', 7))
