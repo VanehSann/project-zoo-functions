b232015b8b086294e338f954870f2180ca0ca5a7
VanehSann <vanessacbsantos22@gmail.com>
2022-01-27 16:32:13 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,24 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined ) {
+   return {}
 }
+// data.employees.forEach((employee) => {
+//   if (employee.firstName === employeeName || employee.lastName === employeeName) {
+//   return employee
+//   }
+// }); 
+
+for(let index = 0; index < data.employees.length; index += 1) {
+
+  if(data.employees[index].firstName === employeeName || data.employees[index].lastName === employeeName) {
+    return data.employees[index]
+  }
+}
+}
+
+console.log(getEmployeeByName('Emery'))
+console.log(getEmployeeByName('Wishart'))
 
 module.exports = getEmployeeByName;
