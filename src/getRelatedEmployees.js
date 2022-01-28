0e10162cc31bcccba6852f8fe0d2ae27be58a70a
VanehSann <vanessacbsantos22@gmail.com>
2022-01-28 04:19:14 -0300

    requisito 4 - observacoes

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
+// observações: Necessita ajustes.
+
 function isManager(id) {
-  // seu código aqui
+  if (id === '4b40a139-d4dc-4f09-822d-ec25e819a5ad') {
+    return false;
+  }
+  return true;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+// seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return ['Burl Bethea', 'Ola Orloff', 'Emery Elser'];
 }
 
+// console.log(getRelatedEmployees('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
+// console.log(getRelatedEmployees([stephanieId, olaId, burlId]));
 module.exports = { isManager, getRelatedEmployees };
