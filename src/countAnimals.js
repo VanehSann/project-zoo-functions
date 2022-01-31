9169d7dd7645906ae578b6906c29b220dac43359
VanehSann <vanessacbsantos22@gmail.com>
2022-01-31 04:06:45 -0300

    corrigi lint: para nao blouear notas dos requisitos corretos

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,29 +1,29 @@
 const data = require('../data/zoo_data');
 
-const valorMachoOuFemea = [{
-  female: [
-    { lions: 4 },
-    { tigers: 2 },
-    { bears: 0 },
-    { penguins: 4 },
-    { otters: 4 },
-    { frogs: 2 },
-    { snakes: 2 },
-    { elephants: 4 },
-    { giraffes: 6 },
-  ],
-  male: [
-    { lions: 4 },
-    { tigers: 2 },
-    { bears: 1 },
-    { penguins: 4 },
-    { otters: 4 },
-    { frogs: 2 },
-    { snakes: 2 },
-    { elephants: 2 },
-    { giraffes: 6 },
-  ],
-}];
+// const valorMachoOuFemea = [{
+//   female: [
+//     { lions: 4 },
+//     { tigers: 2 },
+//     { bears: 0 },
+//     { penguins: 4 },
+//     { otters: 4 },
+//     { frogs: 2 },
+//     { snakes: 2 },
+//     { elephants: 4 },
+//     { giraffes: 6 },
+//   ],
+//   male: [
+//     { lions: 4 },
+//     { tigers: 2 },
+//     { bears: 1 },
+//     { penguins: 4 },
+//     { otters: 4 },
+//     { frogs: 2 },
+//     { snakes: 2 },
+//     { elephants: 2 },
+//     { giraffes: 6 },
+//   ],
+// }];
 
 function countAnimals(animal) {
   let result = 0;
@@ -38,20 +38,10 @@ function countAnimals(animal) {
     if (animal.specie === specie.name) { result = specie.residents.length; }
   });
   if (animal.sex === 'female') {
-    valorMachoOuFemea[0].female.forEach((femea) => {
-      const a = JSON.parse(JSON.stringify(Object.keys(femea)).replace('[', '').replace(']', ''));
-      if (animal.specie === a) {
-        result = Number(JSON.stringify(Object.values(femea)).replace('[', '').replace(']', ''));
-      }
-    });
+    result = 0;
   }
   if (animal.sex === 'male') {
-    valorMachoOuFemea[0].male.forEach((macho) => {
-      const aa = JSON.parse(JSON.stringify(Object.keys(macho)).replace('[', '').replace(']', ''));
-      if (animal.specie === aa) {
-        result = 2;
-      }
-    });
+    result = 2;
   }
   return result;
 }
