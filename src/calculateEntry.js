0cd5ec87ebe1a6b69525bd9f5f862e3d89a23fae
VanehSann <vanessacbsantos22@gmail.com>
2022-02-04 09:47:15 -0300

    requisito 6.1 + lint

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -3,18 +3,18 @@ const data = require('../data/zoo_data');
 function countEntrants(entrants) {
   // seu código aqui
   let countChild = 0;
-  let countAdult = 0; 
+  let countAdult = 0;
   let countSenior = 0;
-entrants.forEach(entrant => {
-  if (entrant.age < 18) {
-    countChild += 1;
-  } else if (entrant.age >= 18 && entrant.age < 50) {
-    countAdult += 1;
-  } else if (entrant.age >= 50) {
-    countSenior += 1;
-  }
-});
-return { adult: countAdult, child: countChild, senior: countSenior }
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) {
+      countChild += 1;
+    } else if (entrant.age >= 18 && entrant.age < 50) {
+      countAdult += 1;
+    } else if (entrant.age >= 50) {
+      countSenior += 1;
+    }
+  });
+  return { adult: countAdult, child: countChild, senior: countSenior };
 }
 
 function calculateEntry(entrants) {
