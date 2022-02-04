b88f90738c16d694bd356773ba7f3110ef2e863e
VanehSann <vanessacbsantos22@gmail.com>
2022-02-04 08:42:08 -0300

    modificando e refatorando requisito 5 - andamento

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
+// criando funcionao  que procura elemento
+function seTiverVazio() {
+  const arrayObjeto = {};
+  for (let index = 0; index < data.species.length; index += 1) {
+    arrayObjeto[`${data.species[index].name}`] = data.species[index].residents.length;
+  }
+  return arrayObjeto;
+}
 function countAnimals(animal) {
-
+  if (animal === undefined) {
+    return seTiverVazio();
+  }
+  let result = 0;
+  data.species.forEach((specie) => {
+    if (animal.specie === specie.name) {
+      result = specie.residents.length;
+    } else if (animal.sex === 'female') {
+      result = 0;
+    } else if (animal.sex === 'male') {
+      result = 2;
+    }
+  });
+  return result;
 }
 
 module.exports = countAnimals;
