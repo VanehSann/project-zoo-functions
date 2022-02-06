ed7986ea3d9b16b4629ab8c2451bcc95d4fbb17d
VanehSann <vanessacbsantos22@gmail.com>
2022-02-06 07:08:12 -0300

    requisito 8 -lint

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -2,6 +2,9 @@ const data = require('../data/zoo_data');
 
 function getAnimalMap(options) {
   // seu código aqui
+  if (options === undefined) {
+  // vazio
+  }
 }
 
 module.exports = getAnimalMap;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,73 @@
 const data = require('../data/zoo_data');
 
+const expected = {
+  'Tuesday': {
+    'officeHour': 'Open from 8am until 6pm',
+    'exhibition': [ 'lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes' ],
+  },
+  'Wednesday': {
+    'officeHour': 'Open from 8am until 6pm',
+    'exhibition': [ 'tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes' ],
+  },
+  'Thursday': {
+    'officeHour': 'Open from 10am until 8pm',
+    'exhibition': [ 'lions', 'otters', 'frogs', 'snakes', 'giraffes' ],
+  },
+  'Friday': {
+    'officeHour': 'Open from 10am until 8pm',
+    'exhibition': [ 'tigers', 'otters', 'frogs', 'snakes', 'elephants', 'giraffes' ],
+  },
+  'Saturday': {
+    'officeHour': 'Open from 8am until 10pm',
+    'exhibition': [
+      'lions',  'tigers',
+      'bears',  'penguins',
+      'otters', 'frogs',
+      'snakes', 'elephants',
+    ],
+  },
+  'Sunday': {
+    'officeHour': 'Open from 8am until 8pm',
+    'exhibition': [ 'lions', 'bears', 'penguins', 'snakes', 'elephants' ],
+  },
+  'Monday': { 'officeHour': 'CLOSED', 'exhibition': 'The zoo will be closed!' },
+};
+const diasDaSemana = Object.keys(data.hours);
+const nomeDosAnimais = [ 'lions', 'tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes' ];
+
+function condicao(scheduleTarget) {
+  const novoArray = {}
+  for (let dia = 0; dia < diasDaSemana.length; dia += 1) {
+    if (scheduleTarget === diasDaSemana[dia]) {
+     novoArray[diasDaSemana[dia]] = expected[diasDaSemana[dia]]
+     return novoArray;
+    }
+  }
+}
+function condicaoDois(scheduleTarget) {
+  for (let dia = 0; dia < diasDaSemana.length; dia += 1) {
+    if (scheduleTarget === data.species[dia].name) {
+     return data.species[dia].availability;
+    }
+  }
+}
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  let result = 0;
+  for (let dia = 0; dia < diasDaSemana.length; dia += 1) {
+    if (scheduleTarget === diasDaSemana[dia]) {
+   
+      return condicao(scheduleTarget)
+      console.log(condicao(scheduleTarget))
+    } if (scheduleTarget === nomeDosAnimais[dia]) {
+      return condicaoDois(scheduleTarget);   
+    }
+   if (scheduleTarget !== diasDaSemana[dia]) {
+    console.log(diasDaSemana[dia] + ' ' + scheduleTarget)
+      result = expected;
+    }
+
+}
+return result;
 }
 
 module.exports = getSchedule;
